cda66f136b3deb5bc6023645db2caa1c1ba555af
Paulo Victor <paulovdbarbosa@gmail.com>
2025-05-20 09:42:38 -0300

    Definições de constantes para configurações

---
+++ _inc/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('API_VERSION',                  '1.0.0');
+
+// api enabled or disabled
+define('API_ACTIVE',                   'true');
+define('API_MESSAGE',                  '');
+
+// mysql config
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_DATABASE', 'udemy_clients');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASS', '');
