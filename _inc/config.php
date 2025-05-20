c69b1d179b56ee8ad4d0d3cc40ebbe3ea52a11c7
Paulo Victor <paulovdbarbosa@gmail.com>
2025-05-20 14:18:50 -0300

    Endpoint teste para verificação de status da API e testar os dados do Basic Auth

--- _inc/config.php
+++ _inc/config.php
@@ -3,11 +3,11 @@
 define('API_VERSION',                  '1.0.0');
 
 // api enabled or disabled
-define('API_ACTIVE',                   'true');
+define('API_ACTIVE',                   true);
 define('API_MESSAGE',                  '');
 
 // mysql config
 define('MYSQL_HOST', 'localhost');
-define('MYSQL_DATABASE', 'udemy_clients');
+define('MYSQL_DATABASE', 'udemy_clientes');
 define('MYSQL_USER', 'root');
 define('MYSQL_PASS', '');
