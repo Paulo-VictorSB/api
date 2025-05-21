14ef7f685bc1b504e35f6c33c7fc61635aa3ef0a
Paulo Victor dos Santos Barbosa <paulo.vdbarbosa@gmail.com>
2025-05-21 20:20:04 -0300

    correção de pequenos problemas.

--- get_total_males_and_females/index.php
+++ get_total_males_and_females/index.php
@@ -7,20 +7,6 @@ require_once("../_inc/init.php");
 // check if request method is valid
 check_request_method($request_method, 'GET');
 
-// check if this gender is set
-if (!isset($_GET['gender'])) {
-    missing_request_paramter('domain');
-}
-
-// check if this gender is string
-if (!is_string($_GET['gender'])) {
-    invalid_type_paramter('gender', 'string');
-}
-
-$params = [
-    ":gender" => $_GET['gender']
-];
-
 // select cliente with gender
 $results = $db->execute_query(
     "SELECT 
@@ -38,7 +24,5 @@ $res->set_status('success');
 $res->set_response_data($results->results);
 // integration key
 check_integration_key_get();
-
-$res->set_aditional_field('total_clients', $results->affected_rows);
 
 $res->response();
