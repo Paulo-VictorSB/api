c9f168951540fac0cf7acf28eed210ec49db0bf3
Paulo Victor dos Santos Barbosa <paulo.vdbarbosa@gmail.com>
2025-05-21 20:17:38 -0300

    Endpoint para saber quantos clientes temos no total, quantos são homens e mulheres.

---
+++ get_total_males_and_females/index.php
@@ -0,0 +1,44 @@
+<?php
+
+// returns all clients
+
+require_once("../_inc/init.php");
+
+// check if request method is valid
+check_request_method($request_method, 'GET');
+
+// check if this gender is set
+if (!isset($_GET['gender'])) {
+    missing_request_paramter('domain');
+}
+
+// check if this gender is string
+if (!is_string($_GET['gender'])) {
+    invalid_type_paramter('gender', 'string');
+}
+
+$params = [
+    ":gender" => $_GET['gender']
+];
+
+// select cliente with gender
+$results = $db->execute_query(
+    "SELECT 
+        COUNT(*) AS total,
+        COUNT(CASE WHEN sexo = 'm' THEN 1 END) AS males,
+        COUNT(CASE WHEN sexo = 'f' THEN 1 END) AS females
+    FROM clientes"
+);
+
+if ($results->affected_rows == 0) {
+    no_data('This gender is f or m.');
+}
+
+$res->set_status('success');
+$res->set_response_data($results->results);
+// integration key
+check_integration_key_get();
+
+$res->set_aditional_field('total_clients', $results->affected_rows);
+
+$res->response();
